3bf0d832af1a9b0fffc8688e6b6ccaa12dc7bd26
riyo3350g <yechouben@gmail.com>
2025-09-02 13:23:52 +0100

    Fix task model

---
+++ app/models/task_model.py
@@ -0,0 +1,12 @@
+# Task Model
+from sqlalchemy import Column, Integer, String, Boolean
+from app.core.database import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=True)
+    completed = Column(Boolean, default=False)
