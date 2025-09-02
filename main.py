867dda52d0ae8aa5cf05937508e570bd55e7c5fc
riyo3350g <yechouben@gmail.com>
2025-09-02 15:52:11 +0100

    Make main handle user routers

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from app.controllers import task_controller, user_controller
+from app.core.database import Base, engine
 
-app = FastAPI(title="Habit Tracker API")
+# Create tables
+Base.metadata.create_all(bind=engine)
 
+app = FastAPI(title="Habit Tracker API - MVC")
+
+# Include controllers
+app.include_router(task_controller.router)
+app.include_router(user_controller.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Habit Tracker API"}
-
-
+    return {"message": "🚀 Habit Tracker API (MVC) is running"}
