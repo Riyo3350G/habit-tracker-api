fdebabea85d21c873e007376934456d31f03bb94
riyo3350g <yechouben@gmail.com>
2025-09-02 15:08:35 +0100

    Fix user viev

---
+++ app/views/user_view.py
@@ -0,0 +1,18 @@
+# User View
+from pydantic import BaseModel, EmailStr
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+class User(UserBase):
+    id: int
+
+    class Config:
+        from_attributes = True
